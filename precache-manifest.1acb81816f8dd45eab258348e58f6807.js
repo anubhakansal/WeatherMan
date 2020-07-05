66b334193849a4bce9f87dc336a6220f75d08ab2
Anubha Kansal <anubhakansal14@gmail.com>
2020-07-05 18:10:44 +0530

    react aaplication"

---
+++ precache-manifest.1acb81816f8dd45eab258348e58f6807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc55cc2e0b94dac0efaf8567dc94740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caff50a09f188a78f41d",
+    "url": "/static/css/main.95f72c6b.chunk.css"
+  },
+  {
+    "revision": "ccfffb05208ff31f467c",
+    "url": "/static/js/2.629a6f25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629a6f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caff50a09f188a78f41d",
+    "url": "/static/js/main.678a9d89.chunk.js"
+  },
+  {
+    "revision": "7f5e41e61bfd33e50ef6",
+    "url": "/static/js/runtime-main.f6d81452.js"
+  }
+]);
